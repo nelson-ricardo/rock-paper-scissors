42c6569f16418983ce2c2c33728c47ce36c09a06
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-09-21 22:15:35 -0400

    add the function to play rounds

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function getPlayerSelection() {
     return prompt("Rock, paper, scissors?", "rock").toLowerCase();
 }
 
+function round(playerSelection, computerSelection) {
+    switch(true) {
+        case playerSelection === computerSelection: {
+            return 'TIE';
+        }
+        case playerSelection === 'paper' && computerSelection === 'rock':
+        case playerSelection === 'scissors' && computerSelection === 'paper':
+        case playerSelection === 'rock' && computerSelection === 'scissors': {
+            return `YOU WIN!! ${playerSelection} beats ${computerSelection}`;
+        }
+        case computerSelection === 'paper' && playerSelection === 'rock':
+        case computerSelection === 'scissors' && playerSelection === 'paper':
+        case computerSelection === 'rock' && playerSelection === 'scissors': {
+            return `YOU LOSE, ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+}
+
+console.log(round(playerSelection(), computerSelection()));
