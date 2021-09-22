73a5d00f56e80efd309a8b0665956889e1ec6d5f
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-09-21 22:00:55 -0400

    add function for player selection

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function computerPlay() {
+function computerSelection() {
     let options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay());
+function playerSelection() {
+    let selection = prompt("Rock, paper, scissors?", "rock").toLowerCase();
+    
+    let options = ['rock', 'paper', 'scissors'];
+
+    while(!options.includes(selection)) {
+        selection = prompt("Rock, paper, scissors?", "rock").toLowerCase();
+    }
+
+    return selection;
+}
+
