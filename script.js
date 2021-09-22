73b8bf45bb4109803807a99504307c4c991f7dca
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-09-21 23:01:44 -0400

    complete game function

--- script.js
+++ script.js
@@ -21,19 +21,45 @@ function getPlayerSelection() {
 function round(playerSelection, computerSelection) {
     switch(true) {
         case playerSelection === computerSelection: {
-            return 'TIE';
+            return 0;
         }
         case playerSelection === 'paper' && computerSelection === 'rock':
         case playerSelection === 'scissors' && computerSelection === 'paper':
         case playerSelection === 'rock' && computerSelection === 'scissors': {
-            return `YOU WIN!! ${playerSelection} beats ${computerSelection}`;
+            return 1;
         }
         case computerSelection === 'paper' && playerSelection === 'rock':
         case computerSelection === 'scissors' && playerSelection === 'paper':
         case computerSelection === 'rock' && playerSelection === 'scissors': {
-            return `YOU LOSE, ${computerSelection} beats ${playerSelection}`;
+            return 2;
         }
     }
 }
 
-console.log(round(playerSelection(), computerSelection()));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let currentSelections = round(playerSelection(), computerSelection());
+
+        if(currentSelections === 1) {
+            playerScore++;
+            console.log('You won this round!');
+        } else if(currentSelections === 2) {
+            computerScore++;
+            console.log("You lose this round");
+        } else {
+            console.log('This round was a tie');
+        }
+
+        console.table({
+            playerScore,
+            computerScore
+        });
+    }
+
+    
+}
+
+game();
