6b8e74a8eaa99017ddb48bc3fcfb20d767c26827
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-09-22 22:51:39 -0400

    add function to play first to five game

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
-let options = ['rock', 'paper', 'scissors'];
+let options = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-let selectionButtons = document.querySelectorAll('button.selection-btn');
-let currentWin = document.getElementById('current-status');
-let playerScoreDOM = document.getElementById('score-player');
-let computerScoreDOM = document.getElementById('score-computer');
+let selectionButtons = document.querySelectorAll("button.selection-btn");
+let currentWin = document.getElementById("current-status");
+let playerScoreDOM = document.getElementById("score-player");
+let computerScoreDOM = document.getElementById("score-computer");
 
 function computerSelection() {
-    return options[Math.floor(Math.random() * 3)];
+  return options[Math.floor(Math.random() * 3)];
 }
 
 function playerSelection(e) {
-    return e.target.textContent.toLowerCase();
+  return e.target.textContent.toLowerCase();
 }
 
-selectionButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        playRound(playerSelection(e), computerSelection());
-    });
+selectionButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playGame(e);
+  });
 });
 
+function playGame(e) {
+  let winStatus = playRound(playerSelection(e), computerSelection());
 
+  if (winStatus === 1) {
+    playerScore++;
+    playerScoreDOM.textContent = playerScore;
+  } else if (winStatus === 2) {
+    computerScore++;
+    computerScoreDOM.textContent = computerScore;
+  }
+
+  if (playerScore === 5 || computerScore === 5) {
+    currentWin.textContent =
+      playerScore === 5 ? "You Have Won!" : "The Computer Has Won";
+      
+    selectionButtons.forEach((button) => {
+      button.disabled = true;
+    });
+  }
+}
 
 function playRound(playerSelection, computerSelection) {
-    switch(true) {
-        case playerSelection === computerSelection: {
-            currentWin.textContent = "Tie";
-            return 0;
-        }
-        case playerSelection === 'paper' && computerSelection === 'rock':
-        case playerSelection === 'scissors' && computerSelection === 'paper':
-        case playerSelection === 'rock' && computerSelection === 'scissors': {
-            currentWin.textContent = "You Win";
-            return 1;
-        }
-        case computerSelection === 'paper' && playerSelection === 'rock':
-        case computerSelection === 'scissors' && playerSelection === 'paper':
-        case computerSelection === 'rock' && playerSelection === 'scissors': {
-            currentWin.textContent = "You Lose";
-            return 2;
-        }
+  switch (true) {
+    case playerSelection === computerSelection: {
+      currentWin.textContent = "Tie";
+      return 0;
     }
+    case playerSelection === "paper" && computerSelection === "rock":
+    case playerSelection === "scissors" && computerSelection === "paper":
+    case playerSelection === "rock" && computerSelection === "scissors": {
+      currentWin.textContent = "You Win";
+      return 1;
+    }
+    case computerSelection === "paper" && playerSelection === "rock":
+    case computerSelection === "scissors" && playerSelection === "paper":
+    case computerSelection === "rock" && playerSelection === "scissors": {
+      currentWin.textContent = "You Lose";
+      return 2;
+    }
+  }
 }
