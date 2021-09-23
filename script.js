08e04e4e4de89ce0899c3ff11e766c0562253686
Nelson Oliva <nelson.ricardo02@gmail.com>
2021-09-22 22:38:23 -0400

    add current win text to DOM

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 let options = ['rock', 'paper', 'scissors'];
-let buttons = document.querySelectorAll('button.selection-btn');
+let playerScore = 0;
+let computerScore = 0;
+
+let selectionButtons = document.querySelectorAll('button.selection-btn');
+let currentWin = document.getElementById('current-status');
+let playerScoreDOM = document.getElementById('score-player');
+let computerScoreDOM = document.getElementById('score-computer');
 
 function computerSelection() {
     return options[Math.floor(Math.random() * 3)];
 }
 
 function playerSelection(e) {
-    let selection = e.target.textContent.toLowerCase();
-    console.log(selection, selection.length);
+    return e.target.textContent.toLowerCase();
 }
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playRound(playerSelection(), computerSelection());
+selectionButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        playRound(playerSelection(e), computerSelection());
     });
-})
+});
+
+
 
 function playRound(playerSelection, computerSelection) {
     switch(true) {
         case playerSelection === computerSelection: {
+            currentWin.textContent = "Tie";
             return 0;
         }
         case playerSelection === 'paper' && computerSelection === 'rock':
         case playerSelection === 'scissors' && computerSelection === 'paper':
         case playerSelection === 'rock' && computerSelection === 'scissors': {
+            currentWin.textContent = "You Win";
             return 1;
         }
         case computerSelection === 'paper' && playerSelection === 'rock':
         case computerSelection === 'scissors' && playerSelection === 'paper':
         case computerSelection === 'rock' && playerSelection === 'scissors': {
+            currentWin.textContent = "You Lose";
             return 2;
         }
     }
